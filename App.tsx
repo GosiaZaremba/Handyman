0f933086c942738ce881534cd7b645b35a3bb09c
Gosia Zaremba <gosia@szel.net>
2022-11-08 12:36:46 +0100

    init

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {StyleSheet, View} from 'react-native';
+import StorybookUIRoot from './storybook';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+const App: () => Node = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+// export default App;
+
+export default StorybookUIRoot;
---
+++ constants/colors.js
@@ -0,0 +1,18 @@
+export const Colors = {
+  button: {
+    light: '#02a568',
+    dark: '#71bc59',
+  },
+  gold: {
+    light: '#ffaa63',
+    dark: '#ff8e0b',
+  },
+  grey: {
+    light: '#d9d9d9',
+    divider: '#e5e5e5',
+  },
+  font: {
+    green: '#44b35e',
+    grey: '#a6a6a6',
+  },
+};
