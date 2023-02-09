07e88ec3bcdab30c589e84e6d529c32cc338f677
Gosia Zaremba <gosia@szel.net>
2023-02-09 15:40:59 +0100

    2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,26 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@typescript-eslint/recommended', '@react-native-community'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    '@react-native-community',
+    'plugin:react-native/all',
+    'plugin:react/recommended',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'react', 'react-native', 'jest'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
         '@typescript-eslint/no-shadow': ['off'],
         'no-shadow': 'off',
         'no-undef': 'off',
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/ban-types': 'off',
+        'react-native/sort-styles': 'off',
       },
     },
   ],
